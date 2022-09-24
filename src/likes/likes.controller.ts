0fe4f78ec5a749b0113a0a01c17da154b507a99d
Pedro Henrique Maia <phmaia2018@gmail.com>
2022-09-23 21:02:49 -0300

    fix: Add UseGuards in controllers

--- src/likes/likes.controller.ts
+++ src/likes/likes.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { CreateLikeDto } from './dto/create-like.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('likes')
+@UseGuards(AuthGuard('jwt'))
 export class LikesController {
   constructor(private readonly likesService: LikesService) {}
 
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -6,12 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('posts')
+@UseGuards(AuthGuard('jwt'))
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
